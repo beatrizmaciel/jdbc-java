7ae63264c6780a8e40cbfae8749a17783230a47e
beatrizmaciel <beatrizcbmaciel@gmail.com>
2022-03-09 21:22:21 -0300

    usando AutoCloseable

--- src/TestaInsercaoComParametro.java
+++ src/TestaInsercaoComParametro.java
@@ -11,50 +11,51 @@ public class TestaInsercaoComParametro {
 	public static void main(String[] args) throws SQLException {
 
 		ConnectionFactory factory = new ConnectionFactory();
-		Connection connection = factory.recuperarConexao();
-		
-		connection.setAutoCommit(false); // dessa forma, controlamos o momento do auto commit da aplicação
-		
-		try {
-			PreparedStatement stm = 
-					connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
-			
-			adicionarVariavel("SmartTV", "45 polegadas", stm);
-			adicionarVariavel("Radio", "Radio de bateria", stm);
-		
-			connection.commit();
-			
-			stm.close();
-			connection.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("ROLLBACK EXECUTADO");
-			connection.rollback();
+
+		try (Connection connection = factory.recuperarConexao()) {
+
+			connection.setAutoCommit(false); // dessa forma, controlamos o momento do auto commit da aplicação
+
+			// \/ abrimos o parênteses (+ chaves) para especificar um AutoClose
+			try (PreparedStatement stm = connection.prepareStatement(
+					"INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);) {
+				adicionarVariavel("SmartTV", "45 polegadas", stm);
+				adicionarVariavel("Radio", "Radio de bateria", stm);
+
+				connection.commit();
+
+				stm.close();
+				connection.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("ROLLBACK EXECUTADO");
+				connection.rollback();
+			}
 		}
 	}
-	
+
 	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
-		
+
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
-		
-		if(nome.equals("Radio")) {
+
+		if (nome.equals("Radio")) {
 			throw new RuntimeException("Não foi possível adicionar o produto");
 		}
-		
+
 		stm.execute();
-		
-		ResultSet rs = stm.getGeneratedKeys();
-		
-		while(rs.next()) {
-			Integer id = rs.getInt(1);
-			System.out.println("O id criado foi: " + id);
+
+		try (ResultSet rs = stm.getGeneratedKeys()) {
+			while (rs.next()) {
+				Integer id = rs.getInt(1);
+				System.out.println("O id criado foi: " + id);
+			}
 		}
-		rs.close();	
 	}
 }
 
 /**
- * O PreparedStatement protege o banco de dados, deixando o código menos vulnerável.
- * O método commit() verifica as getGeneratedKeys e vai dar o commit na "transação"
+ * O PreparedStatement protege o banco de dados, deixando o código menos
+ * vulnerável. O método commit() verifica as getGeneratedKeys e vai dar o commit
+ * na "transação"
  */
