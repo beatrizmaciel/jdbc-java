274b14de0528e1928508a77ee7f743f0e291a528
beatrizmaciel <beatrizcbmaciel@gmail.com>
2022-03-28 11:55:49 -0300

    criando categorias - DAO e Models

---
+++ src/br/com/bibs/jdbc/TestaListagemDeCategorias.java
@@ -0,0 +1,23 @@
+package br.com.bibs.jdbc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import br.com.bibs.jdbc.dao.CategoriaDAO;
+import br.com.bibs.jdbc.model.Categoria;
+
+public class TestaListagemDeCategorias {
+
+	public static void main(String[] args) throws SQLException {
+		
+		try(Connection connection = new ConnectionFactory().recuperarConexao()){
+			CategoriaDAO categoriaDAO = new CategoriaDAO(connection);
+			List<Categoria> listaDeCategoria = categoriaDAO.listar();
+			listaDeCategoria.stream().forEach(ct -> System.out.println(ct.getNome()));
+		}
+		
+		
+	}
+
+}
