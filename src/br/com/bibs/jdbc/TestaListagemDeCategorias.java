d0eb16e17c6981062670aa57dad68ae0d2a9978c
beatrizmaciel <beatrizcbmaciel@gmail.com>
2022-03-29 11:53:22 -0300

    queries n+1 (acessando categorias)

--- src/br/com/bibs/jdbc/TestaListagemDeCategorias.java
+++ src/br/com/bibs/jdbc/TestaListagemDeCategorias.java
@@ -5,7 +5,9 @@ import java.sql.SQLException;
 import java.util.List;
 
 import br.com.bibs.jdbc.dao.CategoriaDAO;
+import br.com.bibs.jdbc.dao.ProdutoDAO;
 import br.com.bibs.jdbc.model.Categoria;
+import br.com.bibs.jdbc.model.Produto;
 
 public class TestaListagemDeCategorias {
 
@@ -14,7 +16,16 @@ public class TestaListagemDeCategorias {
 		try(Connection connection = new ConnectionFactory().recuperarConexao()){
 			CategoriaDAO categoriaDAO = new CategoriaDAO(connection);
 			List<Categoria> listaDeCategoria = categoriaDAO.listar();
-			listaDeCategoria.stream().forEach(ct -> System.out.println(ct.getNome()));
+			listaDeCategoria.stream().forEach(ct -> {
+				System.out.println(ct.getNome());
+				try {
+					for(Produto produto : new ProdutoDAO(connection).buscar(ct)) {
+						System.out.println(ct.getNome() + " - " + produto.getNome());
+					}
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			});
 		}
 		
 		
